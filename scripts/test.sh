fc6a58636a05ed45d92cce0e5f720bd2bf0790e3
马然 <1476318281@qq.com>
2025-03-24 21:26:20 +0800

    scripts/

---
+++ scripts/test.sh
@@ -0,0 +1,19 @@
+GPU_NUM=$1
+CFG=$2
+DATASETS=$3
+OUTPUT_DIR=$4
+IDIR=$5
+FSHOT=$6
+
+
+
+CUDA_VISIBLE_DEVICES=${GPU_NUM} python main.py \
+        --output_dir ${OUTPUT_DIR} \
+        --eval \
+        -c ${CFG} \
+        --datasets ${DATASETS}  \
+        --save_json \
+        --inference_dir ${IDIR} \
+        --f_shot ${FSHOT} \
+        --pretrain_model_path /path/to/checkpoint.pth \
+        --options text_encoder_type=/path/to/bert-base-uncased
